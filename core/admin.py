8e01a7ccf051b4a2c4325b889ed3ebcb46e6108b
raj11rt <rt3390814@gmail.com>
2025-12-23 23:47:53 +0530

    Register Student, TimetableEntry, Attendance in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Student, TimetableEntry, Attendance
 
-# Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("roll_no", "user", "branch", "semester")
+    search_fields = ("roll_no", "user__username", "user__first_name", "user__last_name")
+
+
+@admin.register(TimetableEntry)
+class TimetableEntryAdmin(admin.ModelAdmin):
+    list_display = ("day", "period", "subject", "teacher", "room")
+    list_filter = ("day", "teacher")
+
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ("student", "timetable_entry", "date", "method", "present")
+    list_filter = ("date", "method", "present")
+
